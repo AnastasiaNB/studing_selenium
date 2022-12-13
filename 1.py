cdd98c0510a88d9f56aff0348605aa939f280935
Anastasia <D:\Mail>
2022-12-13 16:15:01 +0300

    ...

--- 1.py
+++ 1.py
@@ -1,23 +1,23 @@
 from bs4 import BeautifulSoup as bs
-import requests
+from selenium.webdriver.common.by import By
 from selenium.webdriver import Edge
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.edge.options import Options as EdgeOptions
 
-
-driver = Edge()
+options = EdgeOptions()
+options.add_experimental_option('excludeSwitches', ['enable-logging'])
+driver = Edge(options=options)
 url = 'https://emias.info/appointment/create'
 driver.get(url)
-driver.find_element_by_xpath("//div[@class='Fkj7oZ f1y1sl b4SQ_y']/input[@class='P8KZKM' and @type='tel' and @value='' and @size='1' and @placeholder='Например, 7100 0000 0000 0000']").click()
-driver.find_element_by_xpath("//div[@class='Fkj7oZ f1y1sl b4SQ_y']/input[@class='P8KZKM' and @type='tel' and @value='' and @size='1' and @placeholder='Например, 7100 0000 0000 0000']").clear()
-driver.find_element_by_xpath("//div[@class='Fkj7oZ f1y1sl b4SQ_y']/input[@class='P8KZKM' and @type='tel' and @value='' and @size='1' and @placeholder='Например, 7100 0000 0000 0000']").send_keys("1111 1111 1111 1111")
 # <div class="Fkj7oZ f1y1sl b4SQ_y"><input size="1" type="tel" name="policy" placeholder="Например, 7100 0000 0000 0000" class="P8KZKM" value=""></div>
-bdate = {
-    'day': '01',
-    'month': '01',
-    'year': '1999'
-}
-for data in bdate:
-    driver.find_element_by_xpath(f"//div[@class='Fkj7oZ ThvYXi f1y1sl']/input[@class='P8KZKM' and @type={data.key} and @value='']").click()
-    driver.find_element_by_xpath(f"//div[@class='Fkj7oZ ThvYXi f1y1sl']/input[@class='P8KZKM' and @type={data.key} and @value='']").clear()
-    driver.find_element_by_xpath(f"//div[@class='Fkj7oZ ThvYXi f1y1sl']/input[@class='P8KZKM' and @type={data.key} and @value='']").send_keys(data.value)
+data = {'day': '03', 'month': '03', 'year': '1998', 'policy': '3656 1008 9600 0147'}
+for key in data.keys():
+    driver.find_element(By.NAME, f'{key}').click()
+    driver.find_element(By.NAME, f'{key}').clear()
+    driver.find_element(By.NAME, f'{key}').send_keys(data[key])
+WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CLASS_NAME, 'zhplzA TW_Q0E zP2XPk M50fgr'))).click()
+# <button type="submit" class="zhplzA TW_Q0E zP2XPk M50fgr"><span class="sXdASz"><span class="AuEX2H">Записаться</span></span><div class="v5Rdd4 JX7z9X"><svg class="vNdVC3 d27STn"><use xlink:href="#loader"></use></svg></div></button>
 # <div class="Fkj7oZ ThvYXi f1y1sl"><input type="text" name="day" placeholder="День" class="P8KZKM" value=""><input type="text" name="month" placeholder="Месяц" class="P8KZKM" value=""><input type="text" name="year" placeholder="Год" class="P8KZKM" value=""></div>
+
 
